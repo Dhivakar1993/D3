fec6cd6e25725c5545d274ba9931fc421fa80235
Dhivakar1993 <dhivadit@gmail.com>
2023-11-05 12:46:32 +0530

    update json

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
-let obj1={name:"Dhivakar",age:30}
-let obj2={age:30,name:"Dhivakar"}
-console.log(obj1,obj2)
+let obj1={name:"person1",age:5}
+let obj2={age:5,name:"person1"}
+let json1 = JSON.stringify(obj1)
+let json2 = JSON.stringify(obj2)
+if (json1===json2)
+{
+    console.log("Both are Equal")
+}
+else
+{
+    console.log("both are not equal");
+}
 
 
 var request = new XMLHttpRequest();
